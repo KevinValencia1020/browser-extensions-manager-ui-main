0c058330d6f8881841c6dcbad4adbf35c9264554
Kevin valencia <kevintovarvalencia21@gmail.com>
2026-03-16 00:04:11 -0500

    feat: Creo y estructuro la funcion para renderizar las extensiones segun el filtro

--- main.js
+++ main.js
@@ -7,10 +7,35 @@ const dataJson = async(json) => {
         }
 
         const data = await response.json();
-        // return data;
-        console.log(data);
+        return data;
     } catch (error) {
         console.error('Error al cargar el archivo JSON:', error);
     }
 }
+
+// Funcion para renderizar las cards de las extensiones
+/**
+ * 
+ * @param {Array} extensions 
+ */
+const renderExtensions = (extensions) =>{
+    const container = document.getElementById("extensions-container");
+
+    // Funcion para filtrar las extensiones segun el filtro seleccionado
+    const filteredExtensions = extensions.filter(extension => {
+        switch (extension) {
+            case "all":
+                return true;
+            case "active":
+                return extension.isActive;
+            case "inactive":
+                return !extension.isActive;
+            default:
+                return true;
+        }
+    });
+
+    
+}
 dataJson("./data.json");
+// renderExtensions("all");
